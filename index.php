5329c7ac33b776ff610dc479c0bd8197622be7c2
Kentaro-0122 <65152135+Kentaro-0122@users.noreply.github.com>
2023-01-20 17:46:41 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <title>CSS Sample</title>
+  <link rel="stylesheet" href="/sample.php">
+</head>
+<body>
+<div class="div-css">外部CSSファイル読み込みのサンプルです。</div>
+</body>
+</html>
